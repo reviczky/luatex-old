24821f675a2d1c1856c7661bfcd25d02fede6326
taco <taco@0b2b3880-5936-4365-a048-eb17d2e5a6bf>
2006-09-01 15:30:33 +0000

    io callback code (wip)

--- src/texk/web2c/luatexdir/luastuff.c
+++ src/texk/web2c/luatexdir/luastuff.c
@@ -42,11 +42,43 @@ const char *getS(lua_State * L, void *ud, size_t * size) {
 }
 
 int
-luaopen_callbacks (lua_State *L) 
+texio_print (lua_State *L) {
+  int i, len;
+  const char *st;
+  if (!lua_isstring(L, -1)) {
+    lua_pushstring(L, "no string to print");
+    lua_error(L);
+  }
+  st = lua_tostring(L, -1);
+  zprint (maketexstring(st));
+  return 0; 
+}
+
+int
+texio_printnl (lua_State *L) {
+  int i, len;
+  const char *st;
+  if (!lua_isstring(L, -1)) {
+    lua_pushstring(L, "no string to print");
+    lua_error(L);
+  }
+  st = lua_tostring(L, -1);
+  zprintnl (maketexstring(st));
+  return 0; 
+}
+
+
+static const struct luaL_reg texiolib [] = {
+  {"write", texio_print},
+  {"write_nl", texio_printnl},
+  {NULL, NULL}  /* sentinel */
+};
+
+
+int
+luaopen_texio (lua_State *L) 
 {
-  lua_pushstring(L,"texio");
-  lua_newtable(L);
-  lua_settable(L, -3);
+  luaL_register(L,"texio",texiolib);
   return 1;
 }
 
@@ -120,9 +152,9 @@ void fix_package_path (lua_State *L, char *key, char *ext, int doinit)
       if (allocpath!=NULL) {
 	if (doinit) {
 	  snprintf(allocpath,alloc,"%s;%s%s?.%s;%s%s?%sinit.%s",
-		   path,curstr,rover,curstr,ext,rover,DIR_SEP_STRING,ext);
+		   path,curstr,rover,ext,curstr,rover,DIR_SEP_STRING,ext);
 	} else {
-	  snprintf(allocpath,alloc,"%s;%s%s?.%s", path,curstr,rover,curstr,ext);
+	  snprintf(allocpath,alloc,"%s;%s%s?.%s", path,curstr,rover,ext);
 	}
 	free (path);
 	path = allocpath;
@@ -143,7 +175,7 @@ luainterpreter (int n) {
   luaopen_pdf(L);
   luaopen_tex(L);
   luaopen_unicode(L);
-  luaopen_callbacks(L);
+  luaopen_texio(L);
   fix_package_path(L,"path","lua",1);
 #if defined(_WIN32)
   fix_package_path(L,"cpath","dll",0);
@@ -257,14 +289,24 @@ callbackdefined (int luaid, char *table,char *name) {
   return 0;
 }
 
+#define CALLBACK_FILE           'f'
+#define CALLBACK_BOOLEAN        'b'
+#define CALLBACK_LINE           'l'
+#define CALLBACK_STRNUMBER      's'
+#define CALLBACK_STRING         'S'
+#define CALLBACK_CHARNUM        'c'
+
+
 int
 runcallback (int luaid, char *table,char *name, char *values, ...) {
   va_list vl;
-  int ret;
+  int ret, len;
   int narg,nres;
   int stacktop;
   FILE **readfile;
   char *s;
+  char *ss;
+  int *bufloc;
   //fprintf(stdout,"\nruncallback called for %s.%s\n",table,name);
   va_start(vl, values);
   if (Luas[luaid] != NULL) {
@@ -277,29 +319,29 @@ runcallback (int luaid, char *table,char *name, char *values, ...) {
 	for (narg = 0; *values; narg++) {
 	  luaL_checkstack(Luas[luaid],1,"out of stack space");
 	  switch (*values++) {
-	  case 'f': /* FILE * */ 
+	  case CALLBACK_FILE: /* FILE * */ 
 	    //lua_pushboolean(Luas[luaid], va_arg(vl, int));
 	    readfile = (FILE **)lua_newuserdata(Luas[luaid], sizeof(FILE *));
 	    *readfile = va_arg(vl, FILE *); 
 	    luaL_getmetatable(Luas[luaid], LUA_FILEHANDLE);
 	    lua_setmetatable(Luas[luaid], -2);
 	    break;
-	  case 'c': /* C string */ 
+	  case CALLBACK_CHARNUM: /* C string */ 
 	    s = malloc(2);
 	    snprintf(s,2,"%c",va_arg(vl, int));
 	    lua_pushstring(Luas[luaid], s);
 	    break;
-	  case 'S': /* C string */ 
+	  case CALLBACK_STRING: /* C string */ 
 	    s = va_arg(vl, char *);
-	    lua_pushstring(Luas[luaid], s);
+	    lua_pushstring(Luas[luaid], strdup(s));
 	    break;
-	  case 's': /* TeX string */ 
+	  case CALLBACK_STRNUMBER: /* TeX string */ 
 	    s = makecstring(va_arg(vl, int));
-	    lua_pushstring(Luas[luaid], s);
+	    lua_pushstring(Luas[luaid], strdup(s));
 	    break;
-	  case 'b': /* boolean */ 
-	      lua_pushboolean(Luas[luaid], va_arg(vl, int));
-	      break;
+	  case CALLBACK_BOOLEAN: /* boolean */ 
+	    lua_pushboolean(Luas[luaid], va_arg(vl, int));
+	    break;
 	  case '>': 
 	    goto ENDARGS;
 	  default :
@@ -311,47 +353,62 @@ runcallback (int luaid, char *table,char *name, char *values, ...) {
 	if(lua_pcall(Luas[luaid],narg,nres,0) != 0) {
 	  fprintf(stdout,"This went wrong: %s", lua_tostring(Luas[luaid],-1));
 	  goto EXIT;
-	} else {
-	  if (ret) {
-	    nres = -nres;
-	    while (*values) {
-	      switch (*values++) {
-	      case 'b': 
-		if (!lua_isboolean(Luas[luaid],nres))
-		  goto EXIT;
-		int b = lua_toboolean(Luas[luaid],nres);
-		*va_arg(vl, int *) = b;
-		break;
-	      case 'f': 
-		readfile = (FILE **)lua_touserdata(Luas[luaid], nres);
-		if (*readfile == NULL)
-		  goto EXIT;
-		*va_arg(vl, FILE **) = *readfile;
-		break;
-	      case 'S': 
-		if (!lua_isstring(Luas[luaid],nres))
-		  goto EXIT;
-		s = (char *)lua_tostring(Luas[luaid],nres);
-		if (s==NULL) 
-		  *va_arg(vl, int *) = -1;
-		else  
-		  *va_arg(vl, int *) = maketexstring(s);
-                break;
-	      default: 
-		fprintf(stdout,"invalid value type");
-		goto EXIT;
-	      }
-	      nres++;
+	};
+	nres = -nres;
+	while (*values) {
+	  switch (*values++) {
+	  case CALLBACK_BOOLEAN: 
+	    if (!lua_isboolean(Luas[luaid],nres))
+	      goto EXIT;
+	    int b = lua_toboolean(Luas[luaid],nres);
+	    *va_arg(vl, int *) = b;
+	    break;
+	  case CALLBACK_FILE: 
+	    readfile = (FILE **)lua_touserdata(Luas[luaid], nres);
+	    if (readfile == NULL)
+	      goto EXIT;
+	    *va_arg(vl, FILE **) = *readfile;
+	    break;
+	  case CALLBACK_LINE:  /* TeX line */
+	    if (!lua_isstring(Luas[luaid],nres))
+	      goto EXIT;
+	    ss = (char *)lua_tostring(Luas[luaid],nres);
+	    if (ss!=NULL) {
+	      s = strdup(ss);
+	      bufloc = va_arg(vl, int *);
+	      ret = *bufloc;
+	      len = strlen(s);
+	      check_buf ((*bufloc) + ret,bufsize);
+	      while (len--)
+		buffer[(*bufloc)++] = *s++;
+	      while ((*bufloc)-1>ret && buffer[(*bufloc)-1] == ' ')
+		(*bufloc)--;
 	    }
-	    lua_settop(Luas[luaid],stacktop); // pop the table
-	    return 1;
+	    break;
+	  case CALLBACK_STRNUMBER:  /* TeX string */
+	    if (!lua_isstring(Luas[luaid],nres))
+	      goto EXIT;
+	    s = (char *)lua_tostring(Luas[luaid],nres);
+	    if (s==NULL) 
+	      *va_arg(vl, int *) = -1;
+	    else {
+	      //puts(s);
+	      *va_arg(vl, int *) = maketexstring(strdup(s));
+	    }
+	    break;
+	  default: 
+	    fprintf(stdout,"invalid value type");
+	    goto EXIT;
 	  }
+	  nres++;
 	}
+	lua_settop(Luas[luaid],stacktop); // pop the table
+	return 1;
       }
     }
+  EXIT:
+    lua_settop(Luas[luaid],stacktop);
   }
- EXIT:
-  lua_settop(Luas[luaid],stacktop);
   return 0;
 }
 
