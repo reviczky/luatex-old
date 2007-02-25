1ec134d66c2678908c7bdc85ac7f7a9a76bb8c0e
taco <taco@0b2b3880-5936-4365-a048-eb17d2e5a6bf>
2007-02-25 17:15:51 +0000

    shortened the error and warning message a bit

--- src/texk/web2c/luatexdir/utils.c
+++ src/texk/web2c/luatexdir/utils.c
@@ -240,8 +240,7 @@ void pdftex_fail (const char *fmt, ...)
     va_list args;
     va_start (args, fmt);
     print_ln ();
-    safe_print ("!luaTeX error: ");
-    safe_print (program_invocation_name);
+    safe_print ("!luaTeX error");
     if (cur_file_name) {
         safe_print (" (file ");
         safe_print (cur_file_name);
@@ -271,7 +270,7 @@ void pdftex_warn (const char *fmt, ...)
     va_list args;
     va_start (args, fmt);
     print_ln ();
-    tex_printf ("luaTeX warning: %s", program_invocation_name);
+    tex_printf ("luaTeX warning");
     if (cur_file_name)
         tex_printf (" (file %s)", cur_file_name);
     tex_printf (": ");
